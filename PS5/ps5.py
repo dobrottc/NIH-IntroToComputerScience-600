cbe4e61547355c47d31259af8d986a43e9b59a12
Lauren Ryan <laurenryan135@gmail.com>
2016-10-12 19:09:58 -0400

    Adding a test case for bonus problems, edits for syntax errors
    Signed-off-by: Lauren Ryan <laurenryan135@gmail.com>

---
+++ PS5/ps5.py
@@ -0,0 +1,83 @@
+# 6.00 Problem Set 5
+# RSS Feed Filter
+
+import feedparser
+import string
+import time
+from project_util import translate_html
+from news_gui import Popup
+
+#-----------------------------------------------------------------------
+#
+# Problem Set 5
+
+#======================
+# Code for retrieving and parsing
+# Google and Yahoo News feeds
+# Do not change this code
+#======================
+
+def process(url):
+    """
+    Fetches news items from the rss url and parses them.
+    Returns a list of NewsStory-s.
+    """
+    feed = feedparser.parse(url)
+    entries = feed.entries
+    ret = []
+    for entry in entries:
+        guid = entry.guid
+        title = translate_html(entry.title)
+        link = entry.link
+        summary = translate_html(entry.summary)
+        try:
+            subject = translate_html(entry.tags[0]['term'])
+        except AttributeError:
+            subject = ""
+        newsStory = NewsStory(guid, title, subject, summary, link)
+        ret.append(newsStory)
+    return ret
+
+#======================
+# Part 1
+# Data structure design
+#======================
+
+# Problem 1
+
+
+# TODO: NewsStory-- write a class Newstory containing various given methods
+
+#======================
+# Part 2
+# Triggers
+#======================
+
+class Trigger(object):
+    def evaluate(self, story):
+        """
+        Returns True if an alert should be generated
+        for the given news item, or False otherwise.
+        """
+        raise NotImplementedError
+
+# Whole Word Triggers
+# Problems 2-5
+
+# TODO: WordTrigger
+
+
+# TODO: TitleTrigger
+# TODO: SubjectTrigger
+# TODO: SummaryTrigger
+
+
+# Composite Triggers
+# Problems 6-8
+
+# TODO: NotTrigger
+# TODO: AndTrigger
+# TODO: OrTrigger
+
+
+
