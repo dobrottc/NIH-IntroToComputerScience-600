912a695912ba00cf56bcd301dba1e2d78daa5fd0
Mark Histed <mhisted@gmail.com>
2016-10-13 22:10:26 -0400

    PS5 answers: mh
    Signed-off-by: Mark Histed <mhisted@gmail.com>

--- PS5/ps5.py
+++ PS5/ps5.py
@@ -4,6 +4,7 @@
 import feedparser
 import string
 import time
+import re
 from project_util import translate_html
 from news_gui import Popup
 
@@ -47,6 +48,23 @@ def process(url):
 
 
 # TODO: NewsStory-- write a class Newstory containing various given methods
+class NewsStory():
+    def __init__(self, guid, title, subject, summary, link):
+        self.guid = guid
+        self.title = title
+        self.subject = subject
+        self.summary = summary
+        self.link = link
+    def get_guid(self):
+        return self.guid
+    def get_title(self):
+        return self.title
+    def get_subject(self):
+        return self.subject
+    def get_summary(self):
+        return self.summary
+    def get_link(self):
+        return self.link
 
 #======================
 # Part 2
@@ -65,12 +83,31 @@ class Trigger(object):
 # Problems 2-5
 
 # TODO: WordTrigger
+class WordTrigger(Trigger):
+    def __init__(self, word):
+        self.word = word.lower()
+    def is_word_in(self, text):
+        wordList = re.split('\W+', text.lower())
+        if self.word in wordList:
+            return True
+        else:
+            return False
+    
 
 
 # TODO: TitleTrigger
+class TitleTrigger(WordTrigger):
+    def evaluate(self, story):
+        return self.is_word_in(story.title.lower())
 # TODO: SubjectTrigger
+class SubjectTrigger(WordTrigger):
+    def evaluate(self, story):
+        return self.is_word_in(story.subject.lower())
 
 # TODO: SummaryTrigger
+class SummaryTrigger(WordTrigger):
+    def evaluate(self, story):
+        return self.is_word_in(story.summary.lower())
 
 
 # TODO: filter_stories
@@ -82,3 +119,9 @@ def filter_stories(stories, triggerlist):
     story.title: story.summary
     """
     ## Write the code here
+    outL = []
+    for story in stories: 
+        for trigger in triggerlist: 
+            if trigger.evaluate(story):
+                outL.append('%s: %s' % (story.title, story.summary))
+    return outL
